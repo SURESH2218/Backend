5d188f9ad54befb9f280b24b6faf482594f153e5
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-08-02 10:28:16 +0530

    imported the app from the app.js correctly

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import express from "express";
 import connectDB from "./db/index.js";
-
-const app = express();
+import app from "./app.js";
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT, () => {
       console.log(`Server is running at port :${process.env.PORT}`);
     });
   })
   .catch((err) => console.log("Mongo db connnection failed", err));
+
+// app.get("/", (req, res) => {
+//   res.send("Server is running");
+// });
+// console.log(process.env.CORS_ORIGIN);
+// console.log(process.env.PORT);
 
 //or
 
